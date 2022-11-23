f87c458b4d73b6371faa334a8d666c979a53af15
Emeka Agara <emekaagara@gmail.com>
2022-11-23 23:16:26 +0100

    home button added

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,11 +1,22 @@
-import { ImageBackground, StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
 const HomeScreen = () => {
   return (
     <View>
       {}
-      <ImageBackground source={require('../../../assets/images/home.jpg')} style={styles.image}></ImageBackground>
+      <ImageBackground source={require('../../../assets/images/home.jpg')} style={styles.image}>
+      <Text style={styles.title}>Book Affordable Apartments</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => console.warn( data='Explore btn')}>
+        <Text style={styles.buttonText}>Get Started</Text>
+
+
+      </TouchableOpacity>
+
+      </ImageBackground>
+      
     </View>
   )
 }
@@ -17,5 +28,30 @@ const styles = StyleSheet.create({
         width:'100%',
         height:500,
         resizeMode:'cover',
+        justifyContent:'center',
+    },
+
+    title:{
+        fontSize:55,
+        fontWeight:'bold',
+        color:'#f1f8ff',
+        width:'80%',
+        marginLeft:25,
+    },
+
+    button:{
+        backgroundColor:'white',
+        width:200,
+        height:55,
+        borderRadius:7,
+        marginTop:25,
+        marginLeft:25,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+
+    buttonText:{
+        fontSize:15,
+        fontWeight:'normal',
     }
 })  
