a4b46feecab1c1b2171f541753e74a39917b6dea
Emeka Agara <emekaagara@gmail.com>
2022-11-24 10:07:53 +0100

    home search button added

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,18 +1,27 @@
-import { ImageBackground, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Dimensions, ImageBackground, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import Feather from "react-native-vector-icons/Feather"
 
 const HomeScreen = () => {
   return (
     <View>
-      {}
+        
+      <TouchableOpacity
+        style={styles.searchButton}
+        onPress={() => console.warn( data='search btn')}>
+        <Feather name="map-pin" size={20} color={"#f15454"}/>
+        <Text style={styles.searchButtonText}>{"  "}Select Location</Text>
+      </TouchableOpacity>
+
+
       <ImageBackground source={require('../../../assets/images/home.jpg')} style={styles.image}>
+      
       <Text style={styles.title}>Book Affordable Apartments</Text>
+      
       <TouchableOpacity
         style={styles.button}
         onPress={() => console.warn( data='Explore btn')}>
         <Text style={styles.buttonText}>Get Started</Text>
-
-
       </TouchableOpacity>
 
       </ImageBackground>
@@ -51,7 +60,28 @@ const styles = StyleSheet.create({
     },
 
     buttonText:{
-        fontSize:15,
-        fontWeight:'normal',
-    }
+      fontSize:17,
+      fontWeight:'bold',
+    },
+
+    searchButton:{
+      backgroundColor:'#EBEBEB',
+      borderColor:'gray',
+      borderWidth:0.5,
+      height:50,
+      flexDirection:'row',
+      borderRadius:7,
+      width:Dimensions.get('screen').width - 40,
+      marginHorizontal:20,
+      justifyContent:'center',
+      alignItems:'center',
+      position:'absolute',
+      zIndex:100,
+      top:20,
+  },
+
+  searchButtonText:{
+    fontSize:17,
+    fontWeight:'500',
+  }
 })  
